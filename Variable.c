2f148118fa9c6c2811e68254cbff66685c9093bd
Avinash_techcoder <avinashkumartiwari7432@gmail.com>
2026-08-28 10:55:55 +0530

    Add files via upload

---
+++ Variable.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int a; // variable decelaration
+    a = 6; // Variable initialization
+
+    printf("The value of the a is %d", a);
+    return 0;
+}
+
+// So variable is a container which stores the value .
---
+++ Variablerules.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int a = 6;
+    int b = 3;
+    int c = 4;
+    int d = 10;
+    int _Avinash = 7;
+    int Avinash = 21;
+    int avinash = 23;
+    int Hey i am =42;  // white space character is not allowed in naming of variable in c.
+    
+}

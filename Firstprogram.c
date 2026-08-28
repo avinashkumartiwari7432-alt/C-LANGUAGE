41f6ea20650fdd9791751acb3cd7eaa17333e649
Avinash_techcoder <avinashkumartiwari7432@gmail.com>
2026-08-28 10:54:04 +0530

    Add files via upload

---
+++ Firstprogram.c
@@ -0,0 +1,7 @@
+#include<stdio.h>   // It is header file.
+
+int main(){     // it is the starting point of c program and execution starts from here.
+    printf("Hello World");
+    return 0;  // program ended successfully.
+}
+// Boilerplate code - That code which must be written .

394f0a34bd09892c6b32b0bf94f609b9953f331f
Avinash_techcoder <avinashkumartiwari7432@gmail.com>
2026-08-30 10:44:38 +0530

    Add files via upload

---
+++ datatypes.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+    // int a;
+    // a = 1;
+
+    int a = 1;  // int stores 4 bytes 
+
+    float b = 3.4;  // float stores 4 bytes
+
+    char c = 'x';  // character stores 1 byte
+
+    // 1 byte = 8 bits and also in 1 bit 0 or 1 stores
+
+}

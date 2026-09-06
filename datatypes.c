97ad864338a70ad26a5b2a7e03e8ec283b8deb0a
Avinash_techcoder <avinashkumartiwari7432@gmail.com>
2026-09-06 09:51:57 +0530

    Update datatypes.c

--- datatypes.c
+++ datatypes.c
@@ -9,8 +9,12 @@ int main(){
 
     float b = 3.4;  // float stores 4 bytes
 
-    char c = 'x';  // character stores 1 byte
+    char g = 'x';  // character stores 1 byte
 
     // 1 byte = 8 bits and also in 1 bit 0 or 1 stores
-
+    printf("The value of a is %d\n", a);
+    printf("The value of b is %f\n", b);
+    printf("The value of g is %c\n", g);
+    return 0;
+    
 }

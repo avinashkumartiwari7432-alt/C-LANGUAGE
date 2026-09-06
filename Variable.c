f60a0bf7671bba5a0fc24032b90aa9144b478183
Avinash_techcoder <avinashkumartiwari7432@gmail.com>
2026-09-06 09:26:51 +0530

    Update Variable.c

--- Variable.c
+++ Variable.c
@@ -3,6 +3,7 @@
 int main(){
     int a; // variable decelaration
     a = 6; // Variable initialization
+    // int a = 6; Varible declaration and intialization.
 
     printf("The value of the a is %d", a);
     return 0;

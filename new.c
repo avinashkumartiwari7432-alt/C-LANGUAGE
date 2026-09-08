991704e21cbececdb5966c8cd4d89e75f36cb33f
Avinash_techcoder <avinashkumartiwari7432@gmail.com>
2026-09-08 07:40:00 +0530

    Update new.c

--- new.c
+++ new.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 
 int main(){
-    printf("size of int: %zu bytes\n", sizeof(int));
-    printf("size of float: %zu bytes\n", sizeof(float));
-    printf("size of char: %zu bytes\n", sizeof(char));
+    printf("size of int is %zu bytes\n", sizeof(int));
+    printf("size of float is %zu bytes\n", sizeof(float));
+    printf("size of char is %zu bytes\n", sizeof(char));
     return 0;
 }
